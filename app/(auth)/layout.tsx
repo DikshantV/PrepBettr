64c15d904f625a2b19374c0d038fa8c2b92382f9
Dikshant Vashistha <dikshantvashistha@macbook-air.local>
2025-05-21 15:20:39 +0530

    final build 1.3.1

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react'
 import {isAuthenticated} from "@/lib/actions/auth.action";
 import {redirect} from "next/navigation";
+import { headers } from 'next/headers';
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
     const isUserAuthenticated = await isAuthenticated();
-
-    if(isUserAuthenticated) redirect("/");
+    const pathname = (await headers()).get('x-invoke-path') || '';
+    if (
+      isUserAuthenticated &&
+      (pathname.endsWith('/sign-in') || pathname.endsWith('/sign-up'))
+    ) {
+      redirect("/");
+    }
     return (
         <div className="auth-layout">{children}</div>
     );
